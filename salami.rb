6e5025d2e2f90fb6fa5f5ae2bf41fb46827d5f2e
petrgazarov <petrgazarov@gmail.com>
2023-10-09 07:51:19 +0000

    Brew formula update for Salami version 0.0.1-rc.1

--- salami.rb
+++ salami.rb
@@ -10,32 +10,48 @@ class Salami < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-darwin-x64"
-      sha256 "f9b497220b938678031963ca3d8b779f7058bad80e7a4320bb44d87b9af4f8b9"
+      sha256 "2f27141c42e2f32dd060625f52207a17b52d98dcd2240938de2a9e322d9543be"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-darwin-arm64"
-      sha256 "f6dee37f2d799a6b2607c0d323f82c85f7f5436e24c8db07950c438e2d2f5398"
+      sha256 "00166c849b35b0817eeabee2d172de00772af9113c75ec4cfa916cf8f25637e4"
     end
 
     def install
-      bin.install Dir["*"]
+      if Hardware::CPU.intel?
+        filename = File.basename("https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-darwin-x64")
+        bin.install filename => "salami"
+      end
+
+      if Hardware::CPU.arm?
+        filename = File.basename("https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-darwin-arm64")
+        bin.install filename => "salami"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-linux-arm64"
-      sha256 "2de4a2ec27f93a799c79eaff629ca6949f99099b61f0b07e637b6f9a20c3e8e4"
+      sha256 "20758a7c42f313d0a6c0487cf15ab85a3d0ff1efb5c1fe6635a7ae96ced1734a"
     end
 
     if Hardware::CPU.intel?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-linux-x64"
-      sha256 "5b59f4c81960056a99ac151b5976e3ef07b336b599139865925f789a754e0f61"
+      sha256 "ef114b2996c99c5cb9af132117fb6c4cbd92ee0b60c4d7f373ffddd24e2640a5"
     end
 
     def install
-      bin.install Dir["*"]
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        filename = File.basename("https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-linux-arm64")
+        bin.install filename => "salami"
+      end
+
+      if Hardware::CPU.intel?
+        filename = File.basename("https://github.com/petrgazarov/salami/releases/download/0.0.1-rc.1/salami-0.0.1-rc.1-linux-x64")
+        bin.install filename => "salami"
+      end
     end
   end
 
