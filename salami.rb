aeb3338855d9641bfb001a301deb1c542cd3507e
petrgazarov <petrgazarov@gmail.com>
2023-10-18 10:22:37 +0000

    Brew formula update for Salami version 0.0.2

--- salami.rb
+++ salami.rb
@@ -10,12 +10,12 @@ class Salami < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.2/salami-0.0.2-darwin-x64"
-      sha256 "ee2e47beca884e3654db66034437eb059c42402d5c60e3a407a4c77263ba5e20"
+      sha256 "eea52754b13a0ad07bd3100b7d15d5d3795d9cb0eebf79dddef147b3e5463edb"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.2/salami-0.0.2-darwin-arm64"
-      sha256 "073777463e37ca6addcff0ab1725ff480676bd926176cf164d47fe152ae9f941"
+      sha256 "f4752fb38c2da12fbac6a154afab1253f3e2daa443cd9e6ef9f4c86f826b2f52"
     end
 
     def install
@@ -34,12 +34,12 @@ class Salami < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.2/salami-0.0.2-linux-arm64"
-      sha256 "0e42529b30654c1db92e6b8a177747fd43d9d958e32a0478715451868e401656"
+      sha256 "eb79a78348408b18a98f4fab8c4bbdf1a5fcafc5f1a586ea446b3ebb78404c40"
     end
 
     if Hardware::CPU.intel?
       url "https://github.com/petrgazarov/salami/releases/download/0.0.2/salami-0.0.2-linux-x64"
-      sha256 "eb3b32e00824bc560fb082a8807e01fb2c1f7c05bb141bdc5020e2ed0213efca"
+      sha256 "0d3e2e5ffb978ed5d51c17dd648560c6d73b36caa71be00df283532e98d41080"
     end
 
     def install
